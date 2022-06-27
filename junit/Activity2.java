c3e0b676d86f399b35728d0d26e4c405fe575f48
Vimal Verma <36690106+vverma8345@users.noreply.github.com>
2022-06-27 10:48:44 +0530

    Add files via upload

---
+++ junit/Activity2.java
@@ -0,0 +1,22 @@
+public class Activity2 {
+
+
+    @Test
+    void notEnoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(9);
+
+        // Assertion for exception
+        assertThrows(NotEnoughFundsException.class, () -> account.withdraw(10),
+                "Balance must be greater than amount of withdrawal");
+    }
+
+    @Test
+    void enoughFunds() {
+        // Create an object for BankAccount class
+        BankAccount account = new BankAccount(100);
+
+        // Assertion for no exceptions
+        assertDoesNotThrow(() -> account.withdraw(100));
+    }
+}
